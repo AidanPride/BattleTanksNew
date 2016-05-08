e1b51250316dd6bf510859a17136ba40a0563d3d
a.aidan@yandex.ru <Aa7030666>
2016-05-08 19:03:07 +0300

    Start GUI

--- game/StartGUI.java
+++ game/StartGUI.java
@@ -10,7 +10,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
+//......
 public class StartGUI extends JPanel {
     private Tank tank;
     private final String T34String = "T34";
